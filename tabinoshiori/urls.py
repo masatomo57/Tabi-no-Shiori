36b9d4d52ca95898e2f8a23045ebf880709bf080
masatomo57 <akagawa.code@gmail.com>
2023-10-01 23:53:17 +0900

    リダイレクトを実装

--- tabinoshiori/urls.py
+++ tabinoshiori/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 app_name = 'tabinoshiori'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/home/')),
     path('home/', views.HomeView, name='home'),
     path('register/trip/', views.RegisterTripView.as_view(), name='registertrip'),
     path('itinerary/<int:pk>/register/', views.RegisterItineraryView, name='registeritinerary'),
